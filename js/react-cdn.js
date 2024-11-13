2350e9e843fe705b7008fedb04574b9cccf45a5a
Reshu Jayant <reshu.22b1531032@abes.ac.in>
2024-11-13 09:54:25 +0530

    REACT

--- js/react-cdn.js
+++ js/react-cdn.js
@@ -1,5 +1,9 @@
 const parent = document.getElementById("root")
 const element=React.createElement("h1",{},"THis is new heading")
+
+const element2 = React.createElement("h2",{},"Shoping cart");
+const item1=React.createElement("li",{},"Iteam-1");
+const item1=React.createElement("li",{},"Iteam-1");
+const list=React.createElement("ul",{},item1,item2);
 const root=ReactDOM.createRoot(parent);
     root.render(element)
-const 
