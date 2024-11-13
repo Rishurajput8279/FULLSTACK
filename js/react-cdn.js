2fe78f00ff750a0464f31a188fa0d1981a7a9a8d
Reshu Jayant <reshu.22b1531032@abes.ac.in>
2024-11-13 09:43:40 +0530

    REACT

---
+++ js/react-cdn.js
@@ -0,0 +1,5 @@
+const parent = document.getElementById("root")
+const element=React.createElement("h1",{},"THis is new heading")
+const root=ReactDOM.createRoot(parent);
+    root.render(element)
+const 
